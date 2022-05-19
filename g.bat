8ac8c3690d0e6f6a6cad49d9f58ef3a69833771c
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-19 12:08:56 -0300

    Final

--- g.bat
+++ g.bat
@@ -5,4 +5,4 @@ git remote add origin https://github.com/corbaz/0K-react-express-heroku.git
 git branch -M main
 git add .
 git commit -am "Final"
-git push -u origin main
+git push -u origin main --force
