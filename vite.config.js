ab6b46182389bfc770b727d12afefb074b1f445d
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-20 12:21:01 -0300

    Final branch github

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+
   server: {
     port: 2736
   },
   preview: {
     port: 2732
   },
+  base: '/0K-react-express-heroku/',
   build: {
     outDir: './dist',
   },
