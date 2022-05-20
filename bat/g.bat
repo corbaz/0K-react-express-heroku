0a7e23e9cf8709429120e76087c394188364bd7e
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-20 12:28:03 -0300

    Final

--- bat/g.bat
+++ bat/g.bat
@@ -1,7 +1,7 @@
 cls
 git rm -r --cached node_modules
 git remote add origin https://github.com/corbaz/0K-react-express-heroku.git
-git branch -M main
+
 git add .
 git commit -am "Final"
-git push -u origin main
+git push -u origin main --force
