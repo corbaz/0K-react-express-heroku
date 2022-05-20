ad1b10429fa6368a9847f264f250311aa0bbe6f6
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-20 15:26:04 -0300

    github: Final deploy Github - Github Pages

---
+++ bat/co.bat
@@ -0,0 +1,4 @@
+cls 
+
+git add .
+git commit -am "github: Final deploy Github - Github Pages"
--- bat/g.bat
+++ bat/g.bat
@@ -2,5 +2,5 @@ cls
 git rm -r --cached node_modules
 git remote add origin https://github.com/corbaz/0K-react-express-heroku.git
 git add .
-git commit -am "Final deploy Github - Github Pages"
+git commit -am "github: Final deploy Github - Github Pages"
 git push -u origin github:main
