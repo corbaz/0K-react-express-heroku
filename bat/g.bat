d18f9fd43e0a38b1fba4cabf4960199a29adef4b
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-20 12:25:27 -0300

    Final deploy Github - Github Pages

--- bat/g.bat
+++ bat/g.bat
@@ -1,7 +1,6 @@
 cls
 git rm -r --cached node_modules
 git remote add origin https://github.com/corbaz/0K-react-express-heroku.git
-git branch -M main
 git add .
-git commit -am "Final"
-git push -u origin main
+git commit -am "Final deploy Github - Github Pages"
+git push -u origin github:main
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,5 @@ export default defineConfig({
   build: {
     outDir: './dist',
   },
-
   plugins: [react()]
 })
